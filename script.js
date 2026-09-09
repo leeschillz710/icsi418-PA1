7cb495e853facf08ab4219ec1df3f349894c6e00
Leigha Schilling <lschilling@albany.edu>
2026-09-08 23:08:17 -0400

    Prepare the JavaScript Structure

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/* reference to form element */
+const form = document.querySelector('form');
+
+/* reference to task input */
+const taskInput = document.querySelector('#task-input');
+
+/* reference to the priority selector */
+const prioritySelector = document.querySelector('#priority-selector'); 
+
+/* reference to the task list */
+const taskList = document.querySelector('#task-list');
+
+tasks = [];
